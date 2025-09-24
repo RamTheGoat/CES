3c1a825e6d9665a9dc36f4150eaa43e47f225d7f
Ychoi001 <131182663+Ychoi001@users.noreply.github.com>
2025-09-24 02:09:55 -0400

    Bones of the Homepage

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,14 @@
 import Header from "../Components/Header";
 import './App.css';
 import '../Components/Header.css';
+import Home from '../Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
           <Header></Header>
+          <Home></Home>
         <p>
         </p>
       </header>
