77ce578d62f43ea26b461dd8bcdd946ba27dfad7
Garrett Lowrance <garrettlowrance12@gmail.com>
2025-11-20 23:58:17 -0500

    fix a bug with the AdminElement

--- src/Components/AdminElement.jsx
+++ src/Components/AdminElement.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 export default function AdminElement(props) {
-    const [user, setUser] = useState({});
-    const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState({});
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        setUser(JSON.parse(localStorage.getItem("user")) ?? {});
-    }, []);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (props.redirect && (!user || user.role !== "admin")) navigate("/");
+    setCurrentUser(user ?? {});
+  }, [navigate, props]);
 
-    if (props.redirect && user.role !== "admin") navigate("/");
-    else return user.role === "admin" ? props.element : <Outlet/>;
+  return currentUser.role === "admin" ? props.element : <Outlet/>;
 }
