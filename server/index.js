162a903881f8d3c30949c2abff21dcdd5dd2a47c
Garrett Lowrance <garrettlowrance12@gmail.com>
2025-11-23 20:14:32 -0500

    server function for getting all users

--- server/index.js
+++ server/index.js
@@ -305,6 +305,16 @@ app.post("/login", async (req, res) => {
 });
 
 
+// GET all user profiles
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 // GET user profile
 app.get("/api/users/:userId", async (req, res) => {
   try {
