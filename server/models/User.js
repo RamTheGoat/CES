dec503ba76662aa887929eb51f0ac73066dd06a8
Ychoi001 <131182663+Ychoi001@users.noreply.github.com>
2025-12-04 01:59:05 -0500

    revert 2

--- server/models/User.js
+++ server/models/User.js
@@ -17,11 +17,7 @@ const userSchema = new mongoose.Schema({
         lastFour: { type: String, required: true },
         expirationMonth: { type: Number, required: true },
         expirationYear: { type: Number, required: true }
-    }], required: false },
-    orderStats: {
-        totalOrders: { type: Number, default: 0 },
-        totalSpent: { type: Number, default: 0 },
-        lastOrderDate: { type: Date } }
+    }], required: false }
 });
 
 const User = mongoose.model("User", userSchema);
